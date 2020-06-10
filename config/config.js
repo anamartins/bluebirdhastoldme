fa6494ff163201e72d09e3aa0dab19b2ca39d8a3
Ana Martins <anacarolcm@gmail.com>
2020-06-10 14:27:04 +0200

    changing api config file

--- config/config.js
+++ config/config.js
@@ -1,11 +1,10 @@
 let API_BASE_URL;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "developmenta") {
   API_BASE_URL = "http://localhost:3000/";
 } else {
   // if node_env is build
-  API_BASE_URL =
-    "http://ec2-18-197-140-221.eu-central-1.compute.amazonaws.com:3000/";
+  API_BASE_URL = "https://api.bluebirdhastoldme.com/villagers/";
 }
 
 export { API_BASE_URL };
