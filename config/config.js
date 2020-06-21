1196baea1a8436b4c7ad2a60c59cbc94f526154c
Ana Martins <anacarolcm@gmail.com>
2020-06-21 20:36:54 +0200

    Fixing IMG BASE URL again. i know.

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === "development") {
   // if node_env is build
   API_BASE_URL = "https://api.bluebirdhastoldme.com/";
   IMG_BASE_URL =
-    "https://s3.eu-central-1.amazonaws.com/www.bluebirdhastoldme.com/";
+    "https://s3.eu-central-1.amazonaws.com/www.bluebirdhastoldme.com";
 }
 
 export { API_BASE_URL, IMG_BASE_URL };
