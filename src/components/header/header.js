1f620e70ab1c6c731c1ca4c38faa3ea2df9631ae
Ana Martins <anacarolcm@gmail.com>
2020-06-26 14:43:05 +0200

    Add about button on header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,17 +3,28 @@ import "./style.scss";
 import logo from "./img/leaf.png";
 import { Link } from "react-router-dom";
 
-function Header() {
-  return (
-    <div className="header">
-      <div className="name">
-        <img src={logo} />
-        <h1>
-          <Link to="/">Blue Bird Has Told Me</Link>
-        </h1>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <div className="name">
+          <img src={logo} />
+          <h1>
+            <Link to="/">Blue Bird Has Told Me</Link>
+          </h1>
+        </div>
+        <div className="button">
+          <Link to="/about">
+            <button type="button">?</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Header;
